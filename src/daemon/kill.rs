517fe11329fe48633a2024689a0ff38abfc24c2a
Clément Nerma <clement.nerma@gmail.com>
2022-11-27 21:08:28 +0100

    Use 'kill -9 -PID' instead of 'pkill -P PID'

--- src/daemon/kill.rs
+++ src/daemon/kill.rs
@@ -3,13 +3,14 @@ use std::process::{Child, Command};
 use anyhow::{bail, Context, Result};
 
 pub fn kill(child: &Child) -> Result<()> {
-    let status = Command::new("pkill")
-        .args(&["-P", &child.id().to_string()])
+    let status = Command::new("kill")
+        .arg("-9")
+        .arg(format!("-{}", child.id()))
         .status()
-        .context("Failed to run the 'pkill' command")?;
+        .context("Failed to run the 'kill' command")?;
 
     if !status.success() {
-        bail!("Command 'pkill' failed");
+        bail!("Command 'kill' failed");
     }
 
     Ok(())
